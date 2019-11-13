2ac0c9c92e2c2cfcac33bcbe3c7d30b216566e65
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-11-13 14:46:06 -0500

    Reviewed Nested Loops and Reading/Writing to Files

--- NestedLoopsAndJavaIOReview/src/com/remijonathan/Main.java
+++ NestedLoopsAndJavaIOReview/src/com/remijonathan/Main.java
@@ -1,8 +1,32 @@
 package com.remijonathan;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //Today's schedule Nested Loops, Objects and Print writer
+        File inputFile = new File("inputFile.txt");
+        File outputFile = new File("outputFile.txt");
 
+        PrintWriter printWriter = new PrintWriter(outputFile);
+
+        Scanner inputData = new Scanner(inputFile);
+
+        while (inputData.hasNextInt()) {
+            int size = inputData.nextInt();
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++)
+                    if (i != j) {
+                        printWriter.print("#");
+                    } else {
+                        printWriter.print("*");
+                    }
+                printWriter.println();
+            }
+            printWriter.println();
+        }
+        printWriter.close();
     }
 }
