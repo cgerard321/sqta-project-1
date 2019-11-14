9e19ddacf0d6abc537241b5073e44a69e00cf53d
Laurent Voisard <laurentvoisard1@gmail.com>
2019-11-14 11:29:47 -0500

    Added a test function to verify that when a user uses the processFunction() with a valid result int hte Binding result returns the right form

---
+++ Team-C/src/main/java/org/springframework/samples/petclinic/owner/PetControllerTest.java
@@ -0,0 +1,35 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class PetControllerTest {
+
+    @Test
+    void processUpdateForm() {
+        //arrange
+        String expectedResult="redirect:/owners/{ownerId}";
+
+        Pet pet = mock(Pet.class);
+        Owner owner = mock(Owner.class);
+        PetRepository pets = mock(PetRepository.class);
+        OwnerRepository owners = mock(OwnerRepository.class);
+        PetController petController = new PetController(pets,owners);
+        ModelMap model = new ModelMap();
+
+        BindingResult bindingResult = mock(BindingResult.class);
+
+        String actualResult;
+        //act
+        when(bindingResult.hasErrors()).thenReturn(false);
+        actualResult = petController.processUpdateForm(pet,bindingResult,owner,model);
+        //assert
+
+        assertEquals(expectedResult,actualResult);
+    }
+}
