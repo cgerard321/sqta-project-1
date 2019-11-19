5734592cd2a778317de218d61952f80d609a37be
Kristian Kurktchiev <kristiankurk@gmail.com>
2019-11-19 13:47:52 -0500

    My second completed test

--- Team-B/src/main/java/org/springframework/samples/petclinic/VetTest.java
+++ Team-B/src/main/java/org/springframework/samples/petclinic/VetTest.java
@@ -1,43 +1,38 @@
 package org.springframework.samples.petclinic;
 
-    import io.github.bonigarcia.seljup.SeleniumExtension;
-    import org.junit.jupiter.api.Test;
-    import org.junit.jupiter.api.extension.ExtendWith;
-    import org.openqa.selenium.By;
-    import org.openqa.selenium.WebElement;
-    import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-    import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SeleniumExtension.class)
-class OwnerTest {
+class VetTest {
     private final String PATH = "C:\\ChromeDriver\\chromedriver.exe";
     private final String BROWSER = "webdriver.chrome.driver";
     private final String SCREENSHOTS = "C:\\ChromeDriver\\Screenshots";
     private ChromeDriver driver;
 
-    public OwnerTest(ChromeDriver driver) {
+    public VetTest(ChromeDriver driver) {
         this.driver = driver;
         System.setProperty(BROWSER,PATH);
     }
 
     @Test
-    void VerifyOpenFindOwners(){
+    void VerifyOpenVets(){
         driver.get("http://localhost:6969");
-        GoToOwnersPage();
+        GoToVetPage();
         pause();
 
         WebElement header = driver.findElement(By.tagName("h2"));
 
-        assertEquals(header.getText().trim(),"Find Owners");
+        assertEquals(header.getText().trim(),"Veterinarians");
     }
-
-
-    void GoToOwnersPage(){
+    void GoToVetPage(){
         driver.get("http://localhost:6969");
-        WebElement ownerlink = driver.findElement(By.xpath("//*[@id=\"main-navbar\"]/ul/li[3]/a"));
+        WebElement vetlink = driver.findElement(By.xpath("//*[@id=\"main-navbar\"]/ul/li[4]/a"));
 
-        ownerlink.click();
+        vetlink.click();
     }
 
     void pause(){
