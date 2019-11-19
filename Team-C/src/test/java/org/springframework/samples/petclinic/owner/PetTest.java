d1d953b59cf55bb99bf88235f0715ab201ffeaa2
Benjamin Caron-Lortie <32286421+ben-cl@users.noreply.github.com>
2019-11-19 13:57:02 -0500

    SQTA-25 V1 Commit (#14)

--- Team-C/src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
+++ Team-C/src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
@@ -6,6 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PetTest {
 
+
+    @Test
+    void set_owner_usingAMock() {
+        //get a mock of the TodoService class
+        //Owner ownerMock = mock(Owner.class);
+
+        Owner ownerTest = new Owner();
+
+        ownerTest.setId(44);
+        ownerTest.setCity("Mtl");
+
+        String expectedResult = "Mtl";
+
+        Pet pet = new Pet();
+        pet.setOwner(ownerTest);
+
+        //Owner ownerActual = pet.getOwner();
+
+        String actualResult = pet.getOwner().getCity();
+        //int actualResult = petMock.getOwner().getId();
+
+        assertEquals(expectedResult,actualResult);
+
+        // set id get id for testing
+
+    }
+
+=======
     //Test to see if we can successfully set and get the right pet type
     @Test
     void getType() {
@@ -28,4 +56,5 @@ class PetTest {
         assertEquals(expectedResult, petTest.getType().getName());
 
     }
+
 }
