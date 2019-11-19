487986221f55f91406b1aa51cff34b98f61b73c0
horiasandu1 <43147197+horiasandu1@users.noreply.github.com>
2019-11-19 14:06:25 -0500

    [SQTA-22] [TEAM C] Completed test method for when there is an error in the method to update the pet.

---
+++ Team-C/src/main/java/org/springframework/samples/petclinic/owner/PetControllerTest.java
@@ -0,0 +1,31 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class PetControllerTest {
+@Test
+    void testPetController_updatePet_withError(){
+    PetRepository pets = mock(PetRepository.class);
+    OwnerRepository owners = mock(OwnerRepository.class);
+
+    PetController petController = new PetController(pets,owners);
+    Pet petMock = mock(Pet.class);
+    Owner ownerMock = mock(Owner.class);
+    BindingResult bResMock = mock(BindingResult.class);
+    ModelMap mMapMock = mock(ModelMap.class);
+
+
+    when(bResMock.hasErrors()).thenReturn(true);
+    String expectedResult = "pets/createOrUpdatePetForm";
+    String actualResult = petController.processUpdateForm(petMock,bResMock,ownerMock,mMapMock);
+    assertEquals(expectedResult,actualResult);
+
+
+
+}
+}
