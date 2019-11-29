66b8e5dc327d26d39e5f4e6ff8f8c4640f6320cd
Karim Ibrahim <40270714+chillman12@users.noreply.github.com>
2019-11-29 11:47:28 -0500

    [TEAM D][SQTA-55] As a user, I can see if an owner exists

--- Team-D/src/test/java/org/springframework/samples/petclinic/visit/VisitTest.java
+++ Team-D/src/test/java/org/springframework/samples/petclinic/visit/VisitTest.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.visit;
 
-
-import io.github.bonigarcia.SeleniumExtension;
+import io.github.bonigarcia.seljup.SeleniumExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
