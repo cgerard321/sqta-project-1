cdab0d5a111f607d4c5c1f40e9f67a31d1a99910
Benjamin Caron-Lortie <32286421+ben-cl@users.noreply.github.com>
2019-12-01 12:17:32 -0500

    complete test case stqa 42

--- Team-C/src/test/java/org/springframework/samples/petclinic/owner/STQA_42_EditOwner.java
+++ Team-C/src/test/java/org/springframework/samples/petclinic/owner/STQA_42_EditOwner.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
+//import static org.junit.jupiter.api.AssertEquals.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @ExtendWith(SeleniumExtension.class)
@@ -31,22 +33,84 @@ public class STQA_42_EditOwner {
         OpenOwnerPage();
         pause();
 
+        // Owner modified
+        Owner georgeEdited = new Owner();
+        georgeEdited.setFirstName("George");
+        georgeEdited.setLastName("Franklin");
+        georgeEdited.setAddress("110 W. Liberty St.");
+        //georgeEdited.setCity("Madison");
+        georgeEdited.setCity("Montreal");
+        georgeEdited.setTelephone("6085551023");
+
+
         //TODO now add method to edit a specific owner
-
-
         //Act
 
         //WebElement findOwnersButton = driver.findElementByXPath("//*[@id=\"search-owner-form\"]/div[2]/div/button");
         WebElement findOwnersButton = driver.findElement(By.cssSelector(".btn.btn-default"));
         findOwnersButton.click();
 
-        WebElement ownersTable = driver.findElementByXPath("//*[@id=\"owners\"]");
+        WebElement openOwner = driver.findElement(By.linkText(georgeEdited.getFirstName() + " " + georgeEdited.getLastName()));
+        //WebElement openOwner = driver.findElement(By.linkText("George Franklin"));
+        //George Franklin
+
+        openOwner.click();
+
+
+        pause();
+
+        WebElement clickEdit =
+            driver.findElement(By.linkText("Edit Owner"));
+        clickEdit.click();
+
+        pause();
+
+
+        // Editing the Owner
+
+        driver.findElement(By.id("address")).clear();
+        driver.findElement(By.id("address")).sendKeys(georgeEdited.getAddress());
+        driver.findElement(By.id("city")).clear();
+        driver.findElement(By.id("city")).sendKeys(georgeEdited.getCity());
+        driver.findElement(By.id("telephone")).clear();
+        driver.findElement(By.id("telephone")).sendKeys(georgeEdited.getTelephone());
+
+        pause();
+
+        //
+        WebElement UpdateOwner =
+            driver.findElement(By.xpath("//*[@id=\"add-owner-form\"]/div[2]/div/button"));
+        UpdateOwner.click();
+
+        pause();
+
+
+        //Get info from
+        WebElement findOwnerFullName =
+            driver.findElement(By.xpath("/html/body/div/div/table[1]/tbody/tr[1]/td/b"));
+        String fullName = findOwnerFullName.getText();
+
+        WebElement findOwnerAddress =
+            driver.findElement(By.xpath("/html/body/div/div/table[1]/tbody/tr[2]/td"));
+        String address = findOwnerAddress.getText();
+
+        WebElement findOwnerCity =
+            driver.findElement(By.xpath("/html/body/div/div/table[1]/tbody/tr[3]/td"));
+        String city = findOwnerCity.getText();
+
+        WebElement findOwnerTelephnone =
+            driver.findElement(By.xpath("/html/body/div/div/table[1]/tbody/tr[4]/td"));
+        String telephone = findOwnerTelephnone.getText();
+
         pause();
 
         //Assert
 
-        assertThat(ownersTable.isDisplayed(), is(true));
-        //assertEquals(header.getText().trim(),"Find Owners");
+        assertEquals(fullName, georgeEdited.getFirstName()+" "+georgeEdited.getLastName());
+        assertEquals(address, georgeEdited.getAddress());
+        assertEquals(city, georgeEdited.getCity());
+        assertEquals(telephone, georgeEdited.getTelephone());
+
     }
 
 
